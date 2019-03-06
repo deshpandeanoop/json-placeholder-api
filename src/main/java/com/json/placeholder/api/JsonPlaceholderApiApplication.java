a906f67c274ca2d78e7de09c4e83120f9b8c94a4
Anoop Deshpande <anoop.deshpande@gmail.com>
2019-03-06 16:16:41 +0530

    Intial commit

---
+++ src/main/java/com/json/placeholder/api/JsonPlaceholderApiApplication.java
@@ -0,0 +1,13 @@
+package com.json.placeholder.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonPlaceholderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonPlaceholderApiApplication.class, args);
+	}
+
+}
